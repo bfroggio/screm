c92bc41f972dfc47c5af8e360946ec8bfdc6163d
Jesse Millar <hellojessemillar@gmail.com>
2020-06-27 13:25:27 -0500

    More files

--- main.go
+++ main.go
@@ -4,9 +4,107 @@ import (
 	"fmt"
 
 	"github.com/MakeNowJust/hotkey"
+
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/hajimehoshi/go-mp3"
+	"github.com/youpy/go-wav"
+
+	"github.com/gen2brain/malgo"
 )
 
 func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("No input file.")
+		os.Exit(1)
+	}
+
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	defer file.Close()
+
+	var reader io.Reader
+	var channels, sampleRate uint32
+
+	switch strings.ToLower(filepath.Ext(os.Args[1])) {
+	case ".wav":
+		w := wav.NewReader(file)
+		f, err := w.Format()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		reader = w
+		channels = uint32(f.NumChannels)
+		sampleRate = f.SampleRate
+
+	case ".mp3":
+		m, err := mp3.NewDecoder(file)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		reader = m
+		channels = 2
+		sampleRate = uint32(m.SampleRate())
+	default:
+		fmt.Println("Not a valid file.")
+		os.Exit(1)
+	}
+
+	ctx, err := malgo.InitContext(nil, malgo.ContextConfig{}, func(message string) {
+		fmt.Printf("LOG <%v>\n", message)
+	})
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer func() {
+		_ = ctx.Uninit()
+		ctx.Free()
+	}()
+
+	deviceConfig := malgo.DefaultDeviceConfig(malgo.Playback)
+	deviceConfig.Playback.Format = malgo.FormatS16
+	deviceConfig.Playback.Channels = channels
+	deviceConfig.SampleRate = sampleRate
+	deviceConfig.Alsa.NoMMap = 1
+
+	// This is the function that's used for sending more data to the device for playback.
+	onSamples := func(pOutputSample, pInputSamples []byte, framecount uint32) {
+		io.ReadFull(reader, pOutputSample)
+	}
+
+	deviceCallbacks := malgo.DeviceCallbacks{
+		Data: onSamples,
+	}
+	device, err := malgo.InitDevice(ctx.Context, deviceConfig, deviceCallbacks)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer device.Uninit()
+
+	err = device.Start()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Press Enter to quit...")
+	fmt.Scanln()
+}
+
+func setupHotkeys() {
 	hkey := hotkey.New()
 
 	quit := make(chan bool)
