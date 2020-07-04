066213458076ddc12afc44a70f0578d76f67b413
Jesse Millar <hellojessemillar@gmail.com>
2020-07-04 13:22:59 -0500

    I can play a hard coded sound effect now

--- go.mod
+++ go.mod
@@ -4,10 +4,8 @@ go 1.14
 
 require (
 	github.com/MakeNowJust/hotkey v0.0.0-20190901154537-dfb5ba96b591
-	github.com/gen2brain/malgo v0.10.5
-	github.com/hajimehoshi/go-mp3 v0.3.0
+	github.com/faiface/beep v1.0.2
+	github.com/hajimehoshi/go-mp3 v0.3.0 // indirect
 	github.com/lxn/win v0.0.0-20191128105842-2da648fda5b4 // indirect
-	github.com/youpy/go-riff v0.0.0-20131220112943-557d78c11efb // indirect
-	github.com/youpy/go-wav v0.0.0-20160223082350-b63a9887d320
 	golang.org/x/sys v0.0.0-20200625212154-ddb9806d33ae // indirect
 )
--- main.go
+++ main.go
@@ -2,106 +2,32 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"time"
 
 	"github.com/MakeNowJust/hotkey"
+	"github.com/faiface/beep/mp3"
+	"github.com/faiface/beep/speaker"
 
-	"io"
 	"os"
-	"path/filepath"
-	"strings"
-
-	"github.com/hajimehoshi/go-mp3"
-	"github.com/youpy/go-wav"
-
-	"github.com/gen2brain/malgo"
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("No input file.")
-		os.Exit(1)
-	}
-
-	file, err := os.Open(os.Args[1])
+	f, err := os.Open("sounds/e-epic/Disney Friend Like Me.mp3")
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		log.Fatal(err)
 	}
 
-	defer file.Close()
-
-	var reader io.Reader
-	var channels, sampleRate uint32
-
-	switch strings.ToLower(filepath.Ext(os.Args[1])) {
-	case ".wav":
-		w := wav.NewReader(file)
-		f, err := w.Format()
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		reader = w
-		channels = uint32(f.NumChannels)
-		sampleRate = f.SampleRate
-
-	case ".mp3":
-		m, err := mp3.NewDecoder(file)
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		reader = m
-		channels = 2
-		sampleRate = uint32(m.SampleRate())
-	default:
-		fmt.Println("Not a valid file.")
-		os.Exit(1)
-	}
-
-	ctx, err := malgo.InitContext(nil, malgo.ContextConfig{}, func(message string) {
-		fmt.Printf("LOG <%v>\n", message)
-	})
+	streamer, format, err := mp3.Decode(f)
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		log.Fatal(err)
 	}
-	defer func() {
-		_ = ctx.Uninit()
-		ctx.Free()
-	}()
+	defer streamer.Close()
 
-	deviceConfig := malgo.DefaultDeviceConfig(malgo.Playback)
-	deviceConfig.Playback.Format = malgo.FormatS16
-	deviceConfig.Playback.Channels = channels
-	deviceConfig.SampleRate = sampleRate
-	deviceConfig.Alsa.NoMMap = 1
+	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
 
-	// This is the function that's used for sending more data to the device for playback.
-	onSamples := func(pOutputSample, pInputSamples []byte, framecount uint32) {
-		io.ReadFull(reader, pOutputSample)
-	}
-
-	deviceCallbacks := malgo.DeviceCallbacks{
-		Data: onSamples,
-	}
-	device, err := malgo.InitDevice(ctx.Context, deviceConfig, deviceCallbacks)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	defer device.Uninit()
-
-	err = device.Start()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	fmt.Println("Press Enter to quit...")
-	fmt.Scanln()
+	speaker.Play(streamer)
+	select {}
 }
 
 func setupHotkeys() {
