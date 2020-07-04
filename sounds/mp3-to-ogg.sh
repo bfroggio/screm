61920abcf2d4be823a7cc2b3777f8680eb8e44e0
Jesse Millar <hellojessemillar@gmail.com>
2020-07-04 13:46:11 -0500

    Tweaking the conversion script

--- sounds/mp3-to-ogg.sh
+++ sounds/mp3-to-ogg.sh
@@ -6,7 +6,6 @@
     for i in *.mp3; do
         j=$(echo -n $i | sed -e 's/.mp3/.ogg/g')
         echo "converting $i to $j"
-        ffmpeg -y -i "$i"  -strict -2 -acodec vorbis -ac 2 -aq 50 "$j"
-        rm "$i"
+        ffmpeg -i "$i" -acodec vorbis -strict -2 "$j" && rm "$i"
     done
 )
