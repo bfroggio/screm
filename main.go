ec34cd4e5b0cc5f27a2f924e4c030b7f5ab3a76d
Jesse Millar <hellojessemillar@gmail.com>
2020-07-04 16:24:17 -0500

    It lives!

--- main.go
+++ main.go
@@ -17,7 +17,34 @@ import (
 func main() {
 	rand.Seed(time.Now().Unix())
 
-	playSfx(getRandomFile("sounds/e-epic"))
+	hkey := hotkey.New()
+
+	quit := make(chan bool)
+
+	hkey.Register(hotkey.Alt, 'Q', func() {
+		fmt.Println("Quit")
+		quit <- true
+	})
+
+	// TODO: Dynamically generate these based on directory structure
+	hkey.Register(hotkey.Alt, 'D', randomSfx("d-defeat"))
+	hkey.Register(hotkey.Alt, 'E', randomSfx("e-epic"))
+	hkey.Register(hotkey.Alt, 'F', randomSfx("f-fail"))
+	hkey.Register(hotkey.Alt, 'G', randomSfx("g-greetings"))
+	hkey.Register(hotkey.Alt, 'J', randomSfx("j-jingles"))
+	hkey.Register(hotkey.Alt, 'M', randomSfx("m-music"))
+	hkey.Register(hotkey.Alt, 'R', randomSfx("r-random"))
+	hkey.Register(hotkey.Alt, 'S', randomSfx("s-success"))
+	hkey.Register(hotkey.Alt, 'T', randomSfx("t-fortnite"))
+	hkey.Register(hotkey.Alt, 'U', randomSfx("u-upset"))
+
+	fmt.Println("Push Alt-Q to escape and quit")
+	<-quit
+}
+
+func randomSfx(directory string) func() {
+	// TODO: Bubble errors
+	return func() { playSfx(getRandomFile("sounds/" + directory)) }
 }
 
 func getRandomFile(directory string) string {
@@ -58,19 +85,4 @@ func playSfx(path string) {
 	})))
 
 	<-done
-}
-
-func setupHotkeys() {
-	hkey := hotkey.New()
-
-	quit := make(chan bool)
-
-	hkey.Register(hotkey.Alt, 'M', func() {
-		fmt.Println("Quit")
-		quit <- true
-	})
-
-	fmt.Println("Start hotkey's loop")
-	fmt.Println("Push Ctrl-Q to escape and quit")
-	<-quit
 }
