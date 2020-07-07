731689a7f8a0d9762d16433eab5b67a8205d53a8
Jesse Millar <jesse.millar@microsoft.com>
2020-07-07 09:17:05 -0500

    Return an error from the config reading function
    Signed-off-by: Jesse Millar <jesse.millar@microsoft.com>

--- main.go
+++ main.go
@@ -29,7 +29,10 @@ var quit = make(chan bool)
 func main() {
 	rand.Seed(time.Now().Unix())
 
-	readConfigFile()
+	err := readConfigFile()
+	if err != nil {
+		log.Fatal("Could not read config file:", err.Error())
+	}
 
 	go func() {
 		err := configureTwitch()
@@ -48,14 +51,16 @@ func main() {
 	<-quit // Keep the program alive until we kill it with a keyboard shortcut
 }
 
-func readConfigFile() {
+func readConfigFile() error {
 	viper.SetConfigName("config") // name of config file (without extension)
 	viper.SetConfigType("toml")   // REQUIRED if the config file does not have the extension in the name
 	viper.AddConfigPath(".")      // optionally look for config in the working directory
 	err := viper.ReadInConfig()   // Find and read the config file
 	if err != nil {               // Handle errors reading the config file
-		log.Fatal("Fatal error reading config file:", err.Error())
+		return err
 	}
+
+	return nil
 }
 
 func configureTwitch() error {
