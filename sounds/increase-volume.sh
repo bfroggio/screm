a1f41e86d2a8b839ec3adbaf63f7c244582277b2
Jesse Millar <jesse.millar@microsoft.com>
2020-07-07 16:52:18 -0500

    Removing a line from the script
    Signed-off-by: Jesse Millar <jesse.millar@microsoft.com>

--- sounds/increase-volume.sh
+++ sounds/increase-volume.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 extension="${1#*.}"
+echo "$extension"
 # ffmpeg -i "$1" -filter:a "volume=1.5" "louder.ogg"
-ffmpeg -i input.wav -filter:a volumedetect -f null /dev/null
 ffmpeg -i "$1" -filter:a loudnorm "louder.$extension"
 mv "louder.$extension" "$1"
