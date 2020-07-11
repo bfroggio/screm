07a2a7d64a3d4fc6e814a4beda85618f9b67c540
Jesse Millar <hellojessemillar@gmail.com>
2020-07-11 11:01:12 -0500

    Simpler script

--- sounds/fix-sample-rate.sh
+++ sounds/fix-sample-rate.sh
@@ -7,14 +7,8 @@
         extension="${filename#*.}"
         short_filename="${filename%.*}"
 
-        if extension="mp3"
-        then
-            ffmpeg -i "$filename" -ar 44100 -vn -c:a libvorbis "out.ogg"
-            mv "out.ogg" "$short_filename.ogg"
-            rm "$filename"
-        else
-            ffmpeg -i "$filename" -ar 44100 "sample-rate-fix.$extension"
-            mv "sample-rate-fix.$extension" "$filename"
-        fi
+        ffmpeg -i "$filename" -ar 44100 -vn -c:a libvorbis "out.ogg"
+        rm "$filename"
+        mv "out.ogg" "$short_filename.ogg"
     done
 )
