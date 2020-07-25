a97592320b3a49bb27d3d66369b6b7c64745e225
Jesse Millar <hellojessemillar@gmail.com>
2020-07-25 12:37:26 -0500

    Lots of organization

--- sounds/decrease-volume.sh
+++ sounds/decrease-volume.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 extension="${1#*.}"
-ffmpeg -i "$1" -filter:a "volume=0.75" "louder.$extension"
+ffmpeg -i "$1" -filter:a "volume=0.5" "louder.$extension"
 mv "louder.$extension" "$1"
--- sounds/increase-volume.sh
+++ sounds/increase-volume.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 extension="${1#*.}"
-ffmpeg -i "$1" -filter:a "volume=1.25" "louder.$extension"
+ffmpeg -i "$1" -filter:a "volume=1.5" "louder.$extension"
 mv "louder.$extension" "$1"
